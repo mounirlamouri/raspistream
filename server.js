dc9333f76f579c2139c99c044c40b81bafd9953c
Mounir Lamouri <mounir@lamouri.fr>
2015-04-06 16:48:13 +0100

    Service ws connection.

--- server.js
+++ server.js
@@ -2,21 +2,47 @@ var http = require('http');
 var url = require('url');
 var path = require('path');
 var fs = require('fs');
+var WebSocketServer = require('websocket').server;
+
+var resources = {
+  '/index.html': { mime: 'text/html' },
+  '/script.js': { mime: 'application/javascript' },
+};
 
 var server = new http.createServer(function(request, response) {
-  console.log((new Date()) + ' Received request for ' + request.url);
+  if (request.url == '/')
+    request.url = '/index.html';
 
-  if (request.url != '/' && request.url != '/index.html') {
+  var resource = resources[request.url];
+  if (!resource) {
+    console.log((new Date()) + ' Invalid request for ' + request.url);
+
     response.writeHead(404);
     response.end();
     return;
   }
 
+  console.log((new Date()) + ' Serving ' + request.url);
+
   var uri = url.parse(request.url).pathname;
-  response.writeHead(200, 'text/html');
+  response.writeHead(200, resource.mime);
   fs.createReadStream(path.join(process.cwd(), uri)).pipe(response);
 });
 
 server.listen(8080, function() {
   console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+ws = new WebSocketServer({
+  httpServer: server,
+  // TODO: add origin checks
+  autoAcceptConnections: true
+});
+
+ws.on('connect', function(connection) {
+  console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' connected.');
+
+  connection.on('close', function(reasonCode, description) {
+      console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
 });
